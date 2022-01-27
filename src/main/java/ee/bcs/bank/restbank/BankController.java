7fe6a6977037df4d458469cc4bf3d3453bcb9df0
Rain Tüür <rain.tuur@gmail.com>
2022-01-27 14:24:34 +0200

    TASK-2 added receiveNewTransaction

--- src/main/java/ee/bcs/bank/restbank/BankController.java
+++ src/main/java/ee/bcs/bank/restbank/BankController.java
@@ -60,6 +60,11 @@ public class BankController {
         return transactionService.addNewTransaction(bank, transactionDto);
     }
 
+    @PostMapping("/receive/transaction")
+    public RequestResult receiveNewTransaction(@RequestBody TransactionDto transactionDto) {
+        return transactionService.receiveNewTransaction(bank, transactionDto);
+    }
+
 
 
     //  loo transactionService alla uus teenus                                      createTransactionForNewAccount()
