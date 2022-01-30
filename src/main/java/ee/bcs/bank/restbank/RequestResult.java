b252f91e070292021a2ecc6a46e58c79467a9ef2
Rain Tüür <rain.tuur@gmail.com>
2022-01-30 10:43:01 +0200

    Master removed all comments

--- src/main/java/ee/bcs/bank/restbank/RequestResult.java
+++ src/main/java/ee/bcs/bank/restbank/RequestResult.java
@@ -3,7 +3,6 @@ package ee.bcs.bank.restbank;
 import lombok.Getter;
 import lombok.Setter;
 
-// Lombok'i annotatsioonid, mis loovad koodi kompileerimise hetkel vajalikud getterid ja setterid
 @Setter
 @Getter
 public class RequestResult {
--- src/main/java/ee/bcs/bank/restbank/TransactionDto.java
+++ src/main/java/ee/bcs/bank/restbank/TransactionDto.java
@@ -5,11 +5,9 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-// Lombok'i annotatsioonid, mis loovad koodi kompileerimise hetkel vajalikud getterid ja setterid
 @Getter
 @Setter
 public class TransactionDto {
-    // DTO - Data Transfer Object
     private int id;
     private int accountId;
     private String senderAccountNumber;
